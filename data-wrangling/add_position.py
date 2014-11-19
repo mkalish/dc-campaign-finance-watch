c4662159bb71487d7c0fedf2d325b2a97d60ced7
mkalish <kalishmichael@gmail.com>
2014-11-18 19:50:58 -0500

    adding positions to candidates

---
+++ data-wrangling/add_position.py
@@ -0,0 +1,38 @@
+#!user/bin/python
+import campaign_mongo_client
+import csv
+path = '../csv/ocf_contributions.csv'
+
+ocf_contributions_field_name = ['committee_name','candidate_name','contributor','address','city','state','zip','contributor_type','contribution_type','employer_name','employer_address','amount','date_of_receipt','office','election_year']
+
+candidate_collection = campaign_mongo_client.get_collection('candidates')
+
+rows = csv.DictReader(open(path), fieldnames=ocf_contributions_field_name)
+next(rows)
+for row in rows:
+    if row['office'] != '':
+        name = row['candidate_name'].split(' ')
+        candidate = candidate_collection.find({'_id.last_name':name[-1].replace('.', ''), '_id.first_name': name[0].replace('.','')})
+
+        if candidate.count() == 1:
+            candidate = candidate[0]
+        elif candidate.count() > 1:
+            for possibility in candidate:
+                    print possibility['_id']
+                    correct = raw_input('Correct: ')
+                    if correct == 'y':
+                        candidate = possibility
+                        break
+            
+        else:
+            print row['candidate_name'] + ' was not found'
+            continue
+ 
+        campaign = {'position':row['office'],'year':row['election_year']}
+
+        if 'campaigns' in candidate:
+            candidate['campaigns'].append(campaign)
+        else:
+             candidate['campaigns'] = [campaign]
+
+        candidate_collection.save(candidate)
